d836a43affc823818a3c57cc5d4d8a0ee21dc103
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-07-25 23:33:30 +0200

    feat: changing the theme of the prism

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,5 @@
 
 // You can delete this file if you're not using it
 import "./src/styles/global.css"
-import "prismjs/themes/prism-solarizedlight.css"
+// import "prismjs/themes/prism-solarizedlight.css"
+import "prismjs/themes/prism-twilight.css"
