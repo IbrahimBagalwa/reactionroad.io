c9b0ccfafe1369e8758e9ba2eb69a27ad825c7f6
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2026-07-22 17:48:59 +0200

    refactor: update Bio component

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -5,10 +5,8 @@ const Bio = () => {
     <div className="flex gap-5 mt-10 items-center mb-6">
       <img src={ibrahimIMG} alt="Ibrahim" className=" w-20 h-20 rounded-full" />
       <div className="text-sm">
-        Welcome to Ibrahim's personal blog, where I delve into the world of
-        technology and beyond. Join me on this captivating journey as I
-        intertwine the power of language and code, unraveling complex concepts
-        with utmost clarity.
+        I&apos;m Ibrahim, and I write about software engineering, practical
+        problem solving, and building reliable systems with clarity.
       </div>
     </div>
   )
